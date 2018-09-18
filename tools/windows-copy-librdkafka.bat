b1fe43fcd3278e5f09f8e1d21db456e1c61c15d5
Magnus Edenhill <magnus@edenhill.se>
2018-09-18 16:35:37 +0200

    wip 71

--- tools/windows-copy-librdkafka.bat
+++ tools/windows-copy-librdkafka.bat
@@ -3,7 +3,7 @@ rem Copy librdkafka headers and libs to Python paths for later use when building
 set librdkafka_version=%1
 set pypath=%2
 
-copy /I /F /S inttypes.h stdint.h %pypath%\include\* || exit /b 1
+xcopy /I /F /S inttypes.h stdint.h %pypath%\include\* || exit /b 1
 echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%\include || exit /b 1
 
 rem Copy x86 libs and dlls
