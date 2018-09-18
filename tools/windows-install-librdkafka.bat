9419cea777ccad9d2e4e8a2ba8366d8c8d564c7c
Magnus Edenhill <magnus@edenhill.se>
2018-09-18 15:02:50 +0200

    wip 65

--- tools/windows-install-librdkafka.bat
+++ tools/windows-install-librdkafka.bat
@@ -13,19 +13,19 @@ rem Download required (but missing) system includes
 curl -s https://raw.githubusercontent.com/chemeris/msinttypes/master/inttypes.h -o inttypes.h
 curl -s https://raw.githubusercontent.com/chemeris/msinttypes/master/stdint.h -o stdint.h
 
+setlocal enabledelayedexpansion
 for %%V in (27, 36, 37) do (
-    set pypath=c:\Python%%~V
-    echo pypath: %pypath%
-    echo pypath2: %%pypath%%
-    copy /I /F /S inttypes.h stdint.h %pypath%\include\*
-    echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%\include
+    pypath=c:\Python%%~V
+    echo pypath: !pypath!
+    copy /I /F /S inttypes.h stdint.h !pypath!\include\*
+    echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* !pypath!\include
 
     rem Copy x86 libs and dlls
-    xcopy /F dest\librdkafka.redist.%librdkafka_version%\build\native\lib\win\x86\win-x86-Release\v120\librdkafka.lib %pypath%\libs\*
-    xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\runtimes\win-x86\native\* %pypath%\libs
+    xcopy /F dest\librdkafka.redist.%librdkafka_version%\build\native\lib\win\x86\win-x86-Release\v120\librdkafka.lib !pypath!\libs\*
+    xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\runtimes\win-x86\native\* !pypath!\libs
 
     rem Copy x64 libs and dlls
-    xcopy /F dest\librdkafka.redist.%librdkafka_version%\build\native\lib\win\x64\win-x64-Release\v120\librdkafka.lib %pypath%-x64\libs\*
-    xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\runtimes\win-x64\native\* %pypath%-x64\libs
+    xcopy /F dest\librdkafka.redist.%librdkafka_version%\build\native\lib\win\x64\win-x64-Release\v120\librdkafka.lib !pypath!-x64\libs\*
+    xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\runtimes\win-x64\native\* !pypath!-x64\libs
 )
 
