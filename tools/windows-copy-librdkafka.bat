0988865d9f03adec373bcf8a5e0dde92cf7bae17
Magnus Edenhill <magnus@edenhill.se>
2018-09-18 20:21:09 +0200

    try without inttypes

--- tools/windows-copy-librdkafka.bat
+++ tools/windows-copy-librdkafka.bat
@@ -8,8 +8,11 @@ copy inttypes.h %pypath%\include\ || exit /b 1
 copy stdint.h %pypath%-x64\include\ || exit /b 1
 copy inttypes.h %pypath%-x64\include\ || exit /b 1
 
-echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%\include || exit /b 1
-echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%-x64\include || exit /b 1
+findstr /V inttypes.h dest\librdkafka.redist.%librdkafka_version%\build\native\include\rdkafka.h > %pypath%\include\rdkafka.h
+findstr /V inttypes.h dest\librdkafka.redist.%librdkafka_version%\build\native\include\rdkafka.h > %pypath%-x64\include\rdkafka.h
+
+rem echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%\include || exit /b 1
+rem echo A|xcopy /I /F /S dest\librdkafka.redist.%librdkafka_version%\build\native\include\* %pypath%-x64\include || exit /b 1
 
 rem Copy x86 libs and dlls
 xcopy /F dest\librdkafka.redist.%librdkafka_version%\build\native\lib\win\x86\win-x86-Release\v120\librdkafka.lib %pypath%\libs\* || exit /b 1
